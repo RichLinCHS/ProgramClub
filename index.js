47d5108a68cd9cd7b1427c4c56a7cabd7ce3015d
Rich Lin <10010884@philasd.org>
2023-09-21 19:41:31 +0000

    created file.js

---
+++ index.js
@@ -0,0 +1,16 @@
+//console.log("hello  world")
+// let, var, const
+//let number=2;
+//number = number *2
+//number *=2
+//console.log(number);
+//if(3>1)
+//console.log("yes")
+//let number=10;
+//if(number>1)
+//console.log('number greater than 1');
+//else if(number<1)
+//console.log('number less than 1');
+//for (let i = 0; i < 10; i++){
+ //   console.log(i**2);
+//}
